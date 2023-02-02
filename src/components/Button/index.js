4a82df2940066a6a83187d7b6c2ea88ac23d3379
katpoola <katriin.poolakese@gmail.com>
2023-02-02 11:48:49 +0200

    Half-finished splash screen

---
+++ src/components/Button/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Pressable, TouchableOpacity, Text } from "react-native";
+import { styles } from "./styles";
+
+const Button = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={styles.container}
+      onPress={onPress}
+    >
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+export default Button;
