cd8c81cea78c7d99e02dbedafa77e18940447376
katpoola <katriin.poolakese@gmail.com>
2023-02-10 14:48:53 +0200

    Created categories box

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../../../components/Header";
+import CategoryBox from "../../../components/CategoryBox";
+
+import { styles } from "./styles";
+import { categories } from "../../../data/categories";
 
 const Home = () => {
+  const renderCategoryItem = ({ item }) => {
+    console.log("item =>", item);
+    return <CategoryBox title={item?.title} image={item?.image} />;
+  };
   return (
     <SafeAreaView>
-      <View>
+      <View style={styles.container}>
+        <Header showSearch={true} title="Find All You Need" />
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          style={styles.list}
+          horizontal
+          data={categories}
+          renderItem={renderCategoryItem}
+          keyExtractor={(item, index) => String(index)}
+        />
         <Text>Home</Text>
       </View>
     </SafeAreaView>
