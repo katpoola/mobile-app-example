40f0371c8fd75b1436d9864f127ba8bd7ca4314c
katpoola <katriin.poolakese@gmail.com>
2023-03-03 09:26:02 +0200

    Create favorites screen

--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./styles";
+import { products } from "../../../data/products";
+import FavoriteItem from "../../../components/FavoriteItem";
+import Header from "../../../components/Header";
 
 const Favorites = () => {
+  const renderItem = ({ item }) => {
+    return <FavoriteItem {...item} />;
+  };
   return (
     <SafeAreaView>
-      <View>
-        <Text>Favorites</Text>
+      <View style={styles.container}>
+        <Header title="Favorites" />
+        <FlatList
+          data={products}
+          renderItem={renderItem}
+          keyExtractor={(item) => String(item.id)}
+        />
       </View>
     </SafeAreaView>
   );
